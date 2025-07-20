818bce4c4ac7f7c0ac353baf3c7460c69cc0dfb7
RaxoTheOne <traxon.bg@gmail.com>
2025-07-21 00:33:52 +0200

    Add initial implementation of Caesar cipher encryption and decryption interface
    - Created index.php with HTML structure and styling
    - Implemented form for text encryption with basic input handling
    - Added toNum function to convert characters to numerical representation
    - Set up basic layout with header and card design

---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Caesar</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+    <style>
+        body {
+            margin: 0;
+            background-color: rgba(0, 0, 0, 0.1);
+        }
+
+        body * {
+            font-family: "Roboto", sans-serif;
+        }
+
+        .header-container {
+            height: 250px;
+            background-color: grey;
+            background-image: url('background.jpg');
+            background-size: cover;
+        }
+
+        .card {
+            height: calc(100vh - 218px);
+            background-color: white;
+            border-radius: 8px;
+            margin-top: -32px;
+            margin-left: 16px;
+            margin-right: 16px;
+            padding: 16px;
+            display: flex;
+            flex-direction: column;
+        }
+
+        form {
+            display: flex;
+            flex-direction: column;
+            margin-bottom: 100px;
+            background-color: rgba(0, 0, 0, 0.05);
+            padding: 16px;
+        }
+
+        input {
+            height: 48px;
+            margin-bottom:32px;
+            border-radius: 8px;
+            border: 1px solid rgba(0, 0, 0, 0.1);
+            background-color: rgba(0, 0, 0, 0.05);
+        }
+
+        button {
+            height: 48px;
+            background-color: #0fab0f;
+            color: white;
+            border: unset;
+            border-radius: 8px;
+            cursor: pointer;
+        }
+
+        button:hover {
+            background-color: #29b929;
+        }
+    </style>
+</head>
+<body>
+    <div class="header-container">
+
+    </div>
+
+    
+    <div class="card">
+        <form>
+            <h2>Text verschlüsseln</h2>
+            <input name="encrypt" placeholder="Hier Text eingeben...">
+            
+            <?php
+                if (isset($_GET['encrypt'])) {
+                    $text = strtolower($_GET['encrypt']);
+                    $array = str_split($text);
+                    foreach ($array as $char) {
+                        echo toNum($char);
+                    }
+                }
+            ?>
+            <button type="submit">VERSCHLÜSSELN</button>
+        </form>
+
+        <form>
+            <h2>Text entschlüsseln</h2>
+            <input name="decrypt" placeholder="Hier Text eingeben...">
+            <button type="submit">ENTSCHLÜSSELN</button>
+        </form>
+    </div>
+</body>
+</html>
+
+<?php
+
+function toNum($data) {
+    $alphabet = array( 'a', 'b', 'c', 'd', 'e',
+                       'f', 'g', 'h', 'i', 'j',
+                       'k', 'l', 'm', 'n', 'o',
+                       'p', 'q', 'r', 's', 't',
+                       'u', 'v', 'w', 'x', 'y',
+                       'z'
+                       );
+    $alpha_flip = array_flip($alphabet);
+    $return_value = -1;
+    $length = strlen($data);
+    for ($i = 0; $i < $length; $i++) {
+        $return_value +=
+            ($alpha_flip[$data[$i]] + 1) * pow(26, ($length - $i - 1));
+    }
+    return $return_value;
+}
+
+?>
